5407431aa34e7986dce256ae85e0768a01e4b465
fernando rioja checa <frioja@epsilon25.aulas.gsyc.es>
2015-02-04 12:12:23 +0100

    Subiendo calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -5,25 +5,29 @@ import sys
 
 """Calculadora sencilla que suma, resta, multiplica y divide"""
 
-def sumar(sumando1, sumando2):
 
+def sumar(sumando1, sumando2):
     """Suma dos enteros o floats"""
     return sumando1 + sumando2
+
 
 def restar(minuendo, sustraendo):
 
     """Resta dos enteros o floats"""
     return minuendo - sustraendo
 
+
 def multiplicar(factor1, factor2):
 
     """Multiplica dos enteros o floats"""
     return factor1 * factor2
 
+
 def dividir(dividendo, divisor):
 
     """Divide dos enteros o floats"""
     return dividendo / divisor
+
 
 if __name__ == "__main__":
     operacion = str(sys.argv[1])
